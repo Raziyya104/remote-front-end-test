4d1e8e05e2c17f5ce85c52d20dc752dff9769820
Lawrence Shaw <lawrence_shaw@Hotmail.Co.uk>
2024-07-07 19:34:22 +0000

    Replacing hard coded data with data from endpoint

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header';
-import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
+import SortAndFilter from '../SortAndFilter';
+import './App.scss';
+
+const api = 'http://localhost:3000/api';
 
 const App = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        if (isLoading) return;
+
+        setIsLoading(true);
+
+        const fetchProperties = async () => {
+            const response = await fetch(`${api}/properties`);
+            const parsedResponse = await response.json();
+
+            setProperties(parsedResponse);
+            setIsLoading(false);
+        };
+
+        fetchProperties();
+    }, []);
+
     return (
         <div className="App">
             <Header />
             <main>
                 <SortAndFilter />
-                <PropertyListing />
+                {properties ? <PropertyListing properties={properties} /> : 'loading...'}
             </main>
         </div>
     );
